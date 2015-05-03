6e2e5361796f40237e2b47e14164bc358a8f31b2
australiansteve <steve@australiansteve.com>
2015-05-03 05:20:31 -0300

    Add full width page template

---
+++ page-templates/full-width.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Template Name: Full width
+ * 
+ * @package Woodstove
+ */
+
+get_header(); ?>
+
+<div class="row"><!-- .row start -->
+
+	<div class="mall-12 columns"><!-- .columns start -->
+
+		<div id="primary" class="content-area">
+			<main id="main" class="site-main" role="main">
+
+				<?php while ( have_posts() ) : the_post(); ?>
+
+					<?php get_template_part( 'page-templates/partials/content', 'page' ); ?>
+
+					<?php
+						// If comments are open or we have at least one comment, load up the comment template
+						if ( comments_open() || get_comments_number() ) :
+							comments_template();
+						endif;
+					?>
+
+				<?php endwhile; // end of the loop. ?>
+
+			</main><!-- #main -->
+		</div><!-- #primary -->
+
+	</div><!-- .columns end -->
+
+</div><!-- .row end -->
+
+<?php get_footer(); ?>
